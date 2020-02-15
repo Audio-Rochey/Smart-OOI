24559d10869ed46882c5e759cadbbb9eab0c34a6
Dafydd Roche <15720888+audio-rochey@users.noreply.github.com>
2020-02-15 14:16:25 -0600

    mask is defined in numpy, white box added in fixed manner, not with mouse etc.

--- sandbox.py
+++ sandbox.py
@@ -6,31 +6,36 @@ import numpy as np
 # from os.path import isfile, join
 
 # black blank image
-blank_image = np.zeros(shape=[960, 1280, 3], dtype=np.uint8)
+#blank_image = np.zeros(shape=[720, 1280, 3], dtype=np.uint8)
+#masker = blank_image
+masker = np.zeros((720, 1280, 1), dtype=np.uint8)
+cv2.rectangle(masker,(100,200),(400,600),(255,255,255),-1)
+# Adds a white rectangle into the mask. (image, start cord, end cord, color, line width where -1 means fill
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
-cap.set(4, 960)
+cap.set(4, 720)
 
 show_mask = 0
 
-# mask = cv2.imread("mask.png",0)
+#masker = cv2.imread("mask.png",0)
 # cv2.namedWindow('ExpatAudio AOI')
 
 while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
 
     char = chr(cv2.waitKey(1) & 255)
     if (char == 'q'):
         break
     elif (char == 27):
         break
-    #elif (char == 't'):
-    #    cv2.imwrite("output.jpg", img)
-    #elif (char == 'd'):
-    #    show_regions = not show_regions
     elif (char == 's'):
+        cv2.imwrite("output.jpg", OutputImage)
+    elif (char == 'l'):
+        masker = cv2.imread("mask.png", 0)
+    elif (char == 'm'):
         show_mask = not show_mask
     #elif (char == 'z'):
     #    zoom = not zoom
@@ -40,14 +45,12 @@ while (True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # diff = cv2.bitwise_and(gray,gray,mask=mask)
-    #diff = gray
-    #diff = blank_image
 
     if show_mask == 0:
         OutputImage = gray
     elif show_mask == 1:
-        OutputImage = blank_image
+        maskedgray = cv2.bitwise_and(gray, gray, mask=masker)
+        OutputImage = maskedgray
 
     # Display the resulting frame
     cv2.imshow('ExpatAudio AOI', OutputImage)
